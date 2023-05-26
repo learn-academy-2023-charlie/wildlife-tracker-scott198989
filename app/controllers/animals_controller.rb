1097c0aa409946e5830aa8440c33bd6c1c8e8c68
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-05-26 14:06:29 -0500

    updated postman actions

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,47 @@
+class AnimalsController < ApplicationController
+
+
+    def index
+        @animals = Animal.all
+        render json: @animals
+      end
+ 
+      def show
+        @animal = Animal.find(params[:id])
+      render json: @animal
+      end
+
+      def create
+        @animal = Animal.create(animal_params)
+        if @animal.valid?
+          render json: @animal
+        else 
+          render json: animal.errors
+        end
+      end
+
+      def update
+        @animal = Animal.find(params[:id])
+        @animal.update(animal_params)
+        if @animal.valid?
+          render json: @animal
+        else
+          render json: @animal.errors
+          end
+        end
+
+        def destroy
+          @animal = Animal.find(params[:id])
+          if @animal.valid?
+            render json: @animal
+            else
+              render json: @animal.errors
+            end
+          end
+
+
+          private
+          def animal_params
+            params.require(:animal).permit(:name, :genus_name)
+          end
+end

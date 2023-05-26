bc1ce65c7bd4d53cdc0f4b41105901c9af4c7af7
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-05-26 12:30:08 -0500

    completed story 1

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,9 @@
 class AnimalsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @animals = Animal.all
+        render json: @animals
+      end
+      
 end
